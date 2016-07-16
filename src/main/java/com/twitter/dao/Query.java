8cf80ff7b9aaa9520d03357270cdce3c1e190b45
msut <marr994@o2.pl>
2016-07-16 18:35:15 +0200

    add some methods to dao

--- src/main/java/com/twitter/dao/Query.java
+++ src/main/java/com/twitter/dao/Query.java
@@ -6,4 +6,7 @@ package com.twitter.dao;
 public final class Query {
     public static final String GET_USER_FOLLOWERS_BY_ID = "SELECT follower FROM User user JOIN user.followers follower WHERE user.id= ?1";
     public static final String GET_USER_FOLLOWING_BY_ID = "SELECT user FROM User user JOIN user.followers follower WHERE follower.id= ?1";
+    public static final String GET_USER_FOLLOWERS_COUNT_BY_ID = "SELECT count(follower) FROM User user JOIN user.followers follower WHERE user.id= ?1";
+    public static final String GET_USER_FOLLOWING_COUNT_BY_ID = "SELECT count(user) FROM User user JOIN user.followers follower WHERE follower.id= ?1";
+
 }
--- src/main/java/com/twitter/dao/UserDao.java
+++ src/main/java/com/twitter/dao/UserDao.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static com.twitter.dao.Query.GET_USER_FOLLOWERS_BY_ID;
-import static com.twitter.dao.Query.GET_USER_FOLLOWING_BY_ID;
+import static com.twitter.dao.Query.*;
 
 /**
  * Created by mariusz on 12.07.16.
@@ -28,4 +27,12 @@ public interface UserDao extends JpaRepository<User, Long> {
 
     @Query(GET_USER_FOLLOWING_BY_ID)
     public List<User> findFollowingByUserId(long userId, Pageable pageable);
+
+    @Query(GET_USER_FOLLOWERS_COUNT_BY_ID)
+    public long findFollowersCountByUserId(long userId);
+
+    @Query(GET_USER_FOLLOWING_COUNT_BY_ID)
+    public long findFollowingCountByUserId(long userId);
+
+    public User findByUsername(String username);
 }
