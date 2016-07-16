3cd418cde648722c25488ac8a98edb7a48a61117
msut <marr994@o2.pl>
2016-07-16 17:01:56 +0200

    add test for getUserById and loadUserByUsername

---
+++ src/test/java/com/twitter/builders/UserBuilder.java
@@ -0,0 +1,106 @@
+package com.twitter.builders;
+
+
+import com.twitter.Builder;
+import com.twitter.model.*;
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by mariusz on 15.07.16.
+ */
+public final class UserBuilder implements Builder<User> {
+    private long id = 0;
+    private Avatar avatar = new Avatar("avatar.jpg", new byte[10]);
+    private String username = "User_" + id;
+    private Password password = new Password("password");
+    private Role role = Role.USER;
+    private Gender gender = Gender.UNDEFINED;
+    private boolean banned = false;
+    private Actions actions = new Actions();
+    private List<User> followers = new ArrayList<>();
+    private DateTime createDate = DateTime.now();
+
+    private UserBuilder() {
+    }
+
+    public static UserBuilder user() {
+        return new UserBuilder();
+    }
+
+    public UserBuilder withAvatar(Avatar avatar) {
+        this.avatar = avatar;
+        return this;
+    }
+
+    public UserBuilder withUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public UserBuilder withPassword(Password password) {
+        this.password = password;
+        return this;
+    }
+
+    public UserBuilder withRole(Role role) {
+        this.role = role;
+        return this;
+    }
+
+    public UserBuilder withGender(Gender gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public UserBuilder withBanned(boolean banned) {
+        this.banned = banned;
+        return this;
+    }
+
+    public UserBuilder withActions(Actions actions) {
+        this.actions = actions;
+        return this;
+    }
+
+    public UserBuilder withFollowers(List<User> followers) {
+        this.followers = followers;
+        return this;
+    }
+
+    public UserBuilder withId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public UserBuilder withCreateDate(DateTime createDate) {
+        this.createDate = createDate;
+        return this;
+    }
+
+    public User build() {
+        User user = new User();
+        user.setAvatar(avatar);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setRole(role);
+        user.setGender(gender);
+        user.setBanned(banned);
+        user.setActions(actions);
+        user.setFollowers(followers);
+        user.setId(id);
+        user.setCreateDate(createDate);
+        return user;
+    }
+
+    public static List<User> aUserListWith(User... users) {
+        return Arrays.asList(users);
+    }
+
+    public static <T> T a(Builder<T> builder) {
+        return builder.build();
+    }
+}
--- src/test/java/com/twitter/service/UserServiceTest.java
+++ src/test/java/com/twitter/service/UserServiceTest.java
@@ -1,13 +1,26 @@
 package com.twitter.service;
 
 import com.twitter.dao.UserDao;
-import org.junit.BeforeClass;
+import com.twitter.exception.UserNotFoundException;
+import com.twitter.model.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static com.twitter.builders.UserBuilder.a;
+import static com.twitter.builders.UserBuilder.user;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by mariusz on 14.07.16.
@@ -16,18 +29,47 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(value = SpringJUnit4ClassRunner.class)
 public class UserServiceTest {
 
+    @Mock
     private UserDao userDao;
 
+    @Autowired
+    @InjectMocks
     private UserService userService;
 
-    @BeforeClass
-    private void setUserDao() {
-        userDao = Mockito.mock(UserDao.class);
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    private void getAllUsersTest(){
-        userService.getAllUsers();
+    public void getUserByIdTest_userExists() {
+        long existingUserId = 1L;
+        User user = a(user().withId(existingUserId));
+        when(userDao.findOne(existingUserId)).thenReturn(user);
+        User userById = userService.getUserById(existingUserId);
+
+        assertThat(userById, is(user));
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    public void getUserByIdTest_userDoesNotExists() {
+        when(userDao.findOne(anyLong())).thenReturn(null);
+        userService.getUserById(1L);
+    }
+
+    @Test
+    public void getUserByUsername_userExists() {
+        String username = "Mariusz";
+        User user = a(user().withUsername(username));
+        when(userDao.findByUsername(username)).thenReturn(user);
+        User userById = userService.loadUserByUsername(username);
+        assertThat(userById, is(user));
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void getUserByUsername_userDoesNotExists() {
+        when(userDao.findByUsername(anyString())).thenReturn(null);
+        userService.loadUserByUsername("Username");
     }
 
 }
