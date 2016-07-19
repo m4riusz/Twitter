2286c6a088f4f8f129ee555ded0cb7b3c9720279
msut <marr994@o2.pl>
2016-07-19 15:11:07 +0200

    add get comments by tweet id method and tests for it

--- src/main/java/com/twitter/dao/TweetDao.java
+++ src/main/java/com/twitter/dao/TweetDao.java
@@ -1,12 +1,22 @@
 package com.twitter.dao;
 
 import com.twitter.model.Tweet;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by mariusz on 19.07.16.
  */
 @Repository
 public interface TweetDao extends JpaRepository<Tweet, Long> {
+
+    public List<Tweet> findByOwnerId(long userId, Pageable pageable);
+
+    @Query("SELECT tweet.comments FROM Tweet tweet WHERE tweet.id = ?1")
+    public List<Tweet> findCommentsById(long tweetId, Pageable pageable);
 }
+
--- src/test/java/com/twitter/dao/TweetDaoTest.java
+++ src/test/java/com/twitter/dao/TweetDaoTest.java
@@ -19,6 +19,7 @@ import static com.twitter.builders.UserBuilder.user;
 import static java.util.Collections.emptyList;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -77,4 +78,56 @@ public class TweetDaoTest {
         assertThat(tweetsFromUserTwo, hasItems(tweet4, tweet5));
         assertThat(tweetsFromUserThree, is(emptyList()));
     }
+
+    @Test
+    public void findCommentsByTweetId_noComments() {
+        User user = a(user());
+        userDao.save(user);
+
+        Tweet tweet = a(tweet().withOwner(user));
+        tweetDao.save(tweet);
+        List<Tweet> commentsFromTweet = tweetDao.findCommentsById(tweet.getId(), new PageRequest(0, 10));
+        assertThat(commentsFromTweet, is(emptyList()));
+    }
+
+    @Test
+    public void findCommentsByTweetId_someComments() {
+        User user = a(user().withUsername("Owner"));
+        User commentator = a(user().withUsername("Commentator"));
+        userDao.save(aListWith(user, commentator));
+
+        Tweet comment = a(tweet().withOwner(commentator));
+        Tweet tweet = a(tweet().withOwner(user).withComments(aListWith(comment)));
+        tweetDao.save(aListWith(comment, tweet));
+        List<Tweet> commentsFromTweet = tweetDao.findCommentsById(tweet.getId(), new PageRequest(0, 10));
+        assertThat(commentsFromTweet, hasItem(comment));
+        assertThat(commentsFromTweet.get(0).getOwner(), is(commentator));
+    }
+
+    @Test
+    public void findCommentsByTweetId_someCommentsAndCommentators() {
+        User userOne = a(user().withUsername("Owner1"));
+        User userTwo = a(user().withUsername("Owner2"));
+        User commentator1 = a(user().withUsername("Commentator1"));
+        User commentator2 = a(user().withUsername("Commentator2"));
+        User commentator3 = a(user().withUsername("Commentator3"));
+        User commentator4 = a(user().withUsername("Commentator4"));
+        userDao.save(aListWith(userOne, userTwo, commentator1, commentator2, commentator3, commentator4));
+
+        Tweet comment1 = a(tweet().withOwner(commentator1));
+        Tweet comment2 = a(tweet().withOwner(commentator2));
+        Tweet comment3 = a(tweet().withOwner(commentator3));
+        Tweet comment4 = a(tweet().withOwner(commentator4));
+        Tweet ownerComment = a(tweet().withOwner(userOne));
+        Tweet tweetFromUserOne = a(tweet().withOwner(userOne).withComments(aListWith(comment1, comment2, comment3, ownerComment)));
+        Tweet tweetFromUserTwo = a(tweet().withOwner(userTwo).withComments(aListWith(comment4)));
+        tweetDao.save(aListWith(comment1, comment2, comment3, comment4, ownerComment, tweetFromUserOne, tweetFromUserTwo));
+
+        List<Tweet> commentsFromTweetOne = tweetDao.findCommentsById(tweetFromUserOne.getId(), new PageRequest(0, 10));
+        List<Tweet> commentsFromTweetTwo = tweetDao.findCommentsById(tweetFromUserTwo.getId(), new PageRequest(0, 10));
+        assertThat(commentsFromTweetOne, hasItems(comment1, comment2, comment3, ownerComment));
+        assertThat(commentsFromTweetTwo, hasItem(comment4));
+    }
+
+
 }
