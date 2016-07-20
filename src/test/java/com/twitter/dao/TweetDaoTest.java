99a765fd64555a8c48074bbf471be4406c184fc0
msut <marr994@o2.pl>
2016-07-20 14:17:40 +0200

    add tests for most popular tweets

--- src/test/java/com/twitter/dao/TweetDaoTest.java
+++ src/test/java/com/twitter/dao/TweetDaoTest.java
@@ -2,6 +2,9 @@ package com.twitter.dao;
 
 import com.twitter.model.Tweet;
 import com.twitter.model.User;
+import com.twitter.model.UserVote;
+import com.twitter.model.Vote;
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +13,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
 import java.util.List;
 
 import static com.twitter.Util.a;
 import static com.twitter.Util.aListWith;
 import static com.twitter.builders.TweetBuilder.tweet;
 import static com.twitter.builders.UserBuilder.user;
+import static com.twitter.builders.UserVoteBuilder.userVote;
 import static java.util.Collections.emptyList;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
@@ -129,5 +134,67 @@ public class TweetDaoTest {
         assertThat(commentsFromTweetTwo, hasItem(comment4));
     }
 
+    @Test
+    public void findMostPopularByVotes_noTweets() {
+        List<Tweet> mostPopular = tweetDao.findMostPopularByVotes(24, new PageRequest(0, 10));
+        assertThat(mostPopular, is(emptyList()));
+    }
+
+    @Test
+    public void findMostPopularByVotes_tooOldTweet() {
+        User user = a(user());
+        userDao.save(aListWith(user));
+        Date tooOldDate = DateTime.now().minusHours(8).minusMinutes(1).toDate();
+        Tweet tweet = a(tweet().withOwner(user).withCreateDate(tooOldDate));
+        tweetDao.save(aListWith(tweet));
+        List<Tweet> mostPopularTweetList = tweetDao.findMostPopularByVotes(8, new PageRequest(0, 10));
+        assertThat(mostPopularTweetList, is(emptyList()));
+    }
+
+    @Test
+    public void findMostPopularByVotes_newTweet() {
+        User user = a(user());
+        userDao.save(aListWith(user));
+        Tweet tweet = a(tweet().withOwner(user));
+        tweetDao.save(aListWith(tweet));
+        List<Tweet> mostPopularTweetList = tweetDao.findMostPopularByVotes(8, new PageRequest(0, 10));
+        assertThat(mostPopularTweetList, hasItem(tweet));
+    }
+
+    @Test
+    public void findMostPopularByVotes_someOldAndNewTweets(){
+
+    }
+
+    @Test
+    public void findMostPopularByVotes_unpopularAndPopularTweets() {
+        User user = a(user());
+        User user1 = a(user().withUsername("voter1"));
+        User user2 = a(user().withUsername("voter2"));
+        User user3 = a(user().withUsername("voter3"));
+
+        userDao.save(aListWith(user, user1, user2, user3));
+        Tweet tweetOne = a(tweet().withOwner(user));
+        Tweet tweetTwo = a(tweet().withOwner(user));
+        Tweet tweetThree = a(tweet().withOwner(user));
+
+        UserVote voteTweetOneUserOne = a(userVote().withUser(user1).withVote(Vote.UP).withTweet(tweetOne));
+        UserVote voteTweetOneUserTwo = a(userVote().withUser(user2).withVote(Vote.UP).withTweet(tweetOne));
+        UserVote voteTweetTwoUserOne = a(userVote().withUser(user1).withVote(Vote.UP).withTweet(tweetTwo));
+        UserVote voteTweetTwoUserTwo = a(userVote().withUser(user2).withVote(Vote.UP).withTweet(tweetTwo));
+        UserVote voteTweetTwoUserThree = a(userVote().withUser(user3).withVote(Vote.UP).withTweet(tweetTwo));
+        UserVote voteTweetThreeUserOne = a(userVote().withUser(user1).withVote(Vote.UP).withTweet(tweetThree));
+
+        tweetOne.setVotes(aListWith(voteTweetOneUserOne, voteTweetOneUserTwo));
+        tweetTwo.setVotes(aListWith(voteTweetTwoUserOne, voteTweetTwoUserTwo, voteTweetTwoUserThree));
+        tweetThree.setVotes(aListWith(voteTweetThreeUserOne));
+        tweetDao.save(aListWith(tweetOne, tweetTwo, tweetThree));
+
+        List<Tweet> mostPopularOnFirstPage = tweetDao.findMostPopularByVotes(24, new PageRequest(0, 2));
+        List<Tweet> mostPopularSecondPage = tweetDao.findMostPopularByVotes(24, new PageRequest(1, 2));
+
+        assertThat(mostPopularOnFirstPage, hasItems(tweetOne, tweetTwo));
+        assertThat(mostPopularSecondPage, hasItem(tweetThree));
+    }
 
 }
