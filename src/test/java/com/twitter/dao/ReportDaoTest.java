9ce99d4bdde0cccf1b061f66bb553e572f0c8dcd
msut <marr994@o2.pl>
2016-07-23 17:41:02 +0200

    add some tests for finding reports with status

--- src/test/java/com/twitter/dao/ReportDaoTest.java
+++ src/test/java/com/twitter/dao/ReportDaoTest.java
@@ -4,6 +4,7 @@ import com.twitter.model.Report;
 import com.twitter.model.ReportStatus;
 import com.twitter.model.Tweet;
 import com.twitter.model.User;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
 import java.util.List;
 
 import static com.twitter.Util.a;
@@ -21,8 +23,7 @@ import static com.twitter.builders.ReportBuilder.report;
 import static com.twitter.builders.TweetBuilder.tweet;
 import static com.twitter.builders.UserBuilder.user;
 import static java.util.Collections.emptyList;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -57,7 +58,7 @@ public class ReportDaoTest {
     }
 
     @Test
-    public void findByStatusOrderByCreateDateAsc_oneReportOneUser() {
+    public void findByStatusOrderByCreateDateAsc_tweetOwnerReportsHimself() {
         User accusedUser = a(user());
         userDao.save(aListWith(accusedUser));
         Report report = a(report().withUser(accusedUser));
@@ -66,6 +67,47 @@ public class ReportDaoTest {
         tweetDao.save(aListWith(tweet));
         List<Report> reportList = reportDao.findByStatusOrderByCreateDateAsc(ReportStatus.WAITING_FOR_REALIZATION, new PageRequest(0, 10));
         assertThat(reportList, hasItem(report));
+    }
+
+    @Test
+    public void findByStatusOrderByCreateDateAsc_manyReportsManyUsers() {
+        User accusedUserOne = a(user());
+        User reportUserOne = a(user());
+        User reportUserTwo = a(user());
+        userDao.save(aListWith(accusedUserOne, reportUserOne, reportUserTwo));
+        Report reportOne = a(report().withStatus(ReportStatus.WAITING_FOR_REALIZATION).withUser(accusedUserOne));
+        Report reportTwo = a(report().withStatus(ReportStatus.GUILTY).withUser(reportUserTwo));
+        Tweet tweet = a(tweet().withOwner(accusedUserOne).withReports(aListWith(reportOne, reportTwo)));
+        reportOne.setAbstractPost(tweet);
+        reportTwo.setAbstractPost(tweet);
+        tweetDao.save(aListWith(tweet));
+        List<Report> reportList = reportDao.findByStatusOrderByCreateDateAsc(ReportStatus.WAITING_FOR_REALIZATION, new PageRequest(0, 10));
+        assertThat(reportList, hasItems(reportOne));
+        assertThat(reportList, not(hasItem(reportTwo)));
+    }
+
+    @Test
+    public void findByStatusOrderByCreateDateAsc_pagingAndOrderTest() {
+        User accusedUserOne = a(user());
+        User reportUserOne = a(user());
+        User reportUserTwo = a(user());
+        User reportUserThree = a(user());
+        userDao.save(aListWith(accusedUserOne, reportUserOne, reportUserTwo, reportUserThree));
+        Date oldestDate = DateTime.now().minusDays(1).toDate();
+        Date normalDate = DateTime.now().toDate();
+        Date youngestDate = DateTime.now().plusDays(1).toDate();
+        Report reportOne = a(report().withUser(accusedUserOne).withCreateDate(oldestDate));
+        Report reportTwo = a(report().withUser(reportUserTwo).withCreateDate(normalDate));
+        Report reportThree = a(report().withUser(reportUserThree).withCreateDate(youngestDate));
+        Tweet tweet = a(tweet().withOwner(accusedUserOne).withReports(aListWith(reportOne, reportTwo, reportThree)));
+        reportOne.setAbstractPost(tweet);
+        reportTwo.setAbstractPost(tweet);
+        reportThree.setAbstractPost(tweet);
+        tweetDao.save(aListWith(tweet));
+        List<Report> reportListPageOne = reportDao.findByStatusOrderByCreateDateAsc(ReportStatus.WAITING_FOR_REALIZATION, new PageRequest(0, 2));
+        List<Report> reportListPageTwo = reportDao.findByStatusOrderByCreateDateAsc(ReportStatus.WAITING_FOR_REALIZATION, new PageRequest(1, 2));
+        assertThat(reportListPageOne, contains(reportOne, reportTwo));
+        assertThat(reportListPageTwo, contains(reportThree));
     }
 
 }
