7ec8de264903378fb52102d6d7b1d6276f18a61b
msut <marr994@o2.pl>
2016-07-27 14:03:55 +0200

    add test for create user, and follow user

--- src/test/java/com/twitter/service/UserServiceTest.java
+++ src/test/java/com/twitter/service/UserServiceTest.java
@@ -1,7 +1,10 @@
 package com.twitter.service;
 
 import com.twitter.dao.UserDao;
+import com.twitter.exception.UserAlreadyExistsException;
+import com.twitter.exception.UserFollowException;
 import com.twitter.exception.UserNotFoundException;
+import com.twitter.model.Result;
 import com.twitter.model.User;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,12 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import static com.twitter.Util.a;
+import static com.twitter.Util.aListWith;
 import static com.twitter.builders.UserBuilder.user;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
-import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.when;
 
 /**
@@ -57,7 +61,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void getUserByUsername_userExists() {
+    public void loadUserByUsername_userExists() {
         String username = "Mariusz";
         User user = a(user().withUsername(username));
         when(userDao.findByUsername(anyString())).thenReturn(user);
@@ -66,10 +70,60 @@ public class UserServiceTest {
     }
 
     @Test(expected = UsernameNotFoundException.class)
-    public void getUserByUsername_userDoesNotExists() {
+    public void loadUserByUsername_userDoesNotExists() {
         when(userDao.findByUsername(anyString())).thenReturn(null);
         User user = userService.loadUserByUsername("Username");
         assertThat(user, is(nullValue()));
     }
 
+    @Test
+    public void create_userCreateTest() {
+        User user = a(user());
+        when(userDao.findByUsername(anyString())).thenReturn(null);
+        when(userDao.save(any(User.class))).thenReturn(user);
+        Result<Boolean> createResult = userService.create(user);
+        assertTrue(createResult.isSuccess());
+        assertThat(createResult.getValue(), is(Boolean.TRUE));
+    }
+
+    @Test(expected = UserAlreadyExistsException.class)
+    public void create_userAlreadyExists() {
+        when(userDao.findByUsername(anyString())).thenReturn(a(user()));
+        userService.create(a(user()));
+    }
+
+    @Test
+    public void follow_userFollowsOtherUser() {
+        User userToFollow = a(user().withId(1L));
+        User user = a(user().withId(2L));
+        when(userDao.findOne(anyLong())).thenReturn(userToFollow);
+        Result<Boolean> followResult = userService.follow(user, userToFollow.getId());
+        assertThat(userToFollow.getFollowers(), hasItem(user));
+        assertThat(followResult.isSuccess(), is(true));
+        assertThat(followResult.getValue(), is(true));
+    }
+
+    @Test(expected = UserFollowException.class)
+    public void follow_userFollowHimself() {
+        User user = a(user());
+        when(userDao.findOne(anyLong())).thenReturn(user);
+        userService.follow(user, user.getId());
+    }
+
+    @Test(expected = UserFollowException.class)
+    public void follow_userAlreadyFollowed() {
+        User user = a(user().withId(1L));
+        User userToFollow = a(user().withId(2L).withFollowers(aListWith(user)));
+        when(userDao.findOne(anyLong())).thenReturn(userToFollow);
+        userService.follow(user, userToFollow.getId());
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    public void follow_userToFollowDoesNotExists() {
+        User user = a(user());
+        when(userDao.findOne(anyLong())).thenReturn(null);
+        userService.follow(user, 1L);
+    }
+
+    
 }
