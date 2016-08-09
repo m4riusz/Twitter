29a8497e9771e4ad8fb303c37a2b32cbc8f46442
msut <marr994@o2.pl>
2016-08-09 22:32:12 +0200

    add profiles util and small changes

---
+++ src/main/java/com/twitter/config/Profiles.java
@@ -0,0 +1,9 @@
+package com.twitter.config;
+
+/**
+ * Created by mariusz on 09.08.16.
+ */
+public final class Profiles {
+    public static final String DEV = "dev";
+    public static final String PRODUCTION = "prod";
+}
