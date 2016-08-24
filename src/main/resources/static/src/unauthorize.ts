126937407ffdca5c1a97b6a8b7a665a8633651da
msut <marr994@o2.pl>
2016-08-24 23:57:16 +0200

    add basic register view

--- src/main/resources/static/src/unauthorize.ts
+++ src/main/resources/static/src/unauthorize.ts
@@ -1,7 +1,21 @@
+import {RouterConfiguration, Router} from "aurelia-router";
 /**
  * Created by mariusz on 24.08.16.
  */
 
-export class UnauthorizeApp{
-    
+export class Unauthorize {
+
+    private router:Router;
+
+    configureRouter(config:RouterConfiguration, router:Router):void {
+        this.router = router;
+        config.title = 'Welcome to twitter!';
+        config.map(
+            [
+                {route: ['', 'login'], name: 'login', moduleId: 'login'},
+                {route: 'register', name: 'register', moduleId: 'register'}
+            ]
+        );
+
+    }
 }
