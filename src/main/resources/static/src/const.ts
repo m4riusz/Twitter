5d3a337e2e0c9907023b0eac0e69ce265e630789
msut <marr994@o2.pl>
2016-08-30 23:57:48 +0200

    add username and password length constraints

--- src/main/resources/static/src/const.ts
+++ src/main/resources/static/src/const.ts
@@ -2,6 +2,14 @@
  * Created by mariusz on 23.08.16.
  */
 export class Const {
+    public static LOGIN_LENGTH = {
+        MIN: 3,
+        MAX: 10
+    }
+    public static PASSWORD_LENGTH = {
+        MIN: 6,
+        MAX: 10
+    }
     public static TOKEN_HEADER:string = 'Auth-Token';
     public static UNAUTHORIZE_ROOT:string = 'unauthorize';
     public static APP_ROOT:string = 'app';
