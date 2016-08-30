ea854e144bd5161843c7bb83f022d92c98efbdf9
msut <marr994@o2.pl>
2016-08-30 23:32:54 +0200

    add register view-model

--- src/main/resources/static/src/authService.ts
+++ src/main/resources/static/src/authService.ts
@@ -1,14 +1,21 @@
 import {Const} from "./const";
 import {Aurelia} from "aurelia-framework";
-import {HttpClient} from "aurelia-fetch-client";
+import {HttpClient, json} from "aurelia-fetch-client";
 import {inject} from "aurelia-dependency-injection";
+import {BASE_URL, LOGIN, REGISTER} from "./route";
+
 /**
  * Created by mariusz on 23.08.16.
  */
 
 
+export interface AuthService {
+    login(username:string, password:string);
+    register(username:string, password:string, email:string, gender:Twitter.Models.Gender);
+}
+
 @inject(Aurelia, HttpClient)
-export class AuthService {
+export class AuthServiceImpl implements AuthService {
 
     private token:string;
     private aurelia:Aurelia;
@@ -17,33 +24,69 @@ export class AuthService {
     constructor(aurelia:Aurelia, httpClient:HttpClient) {
         this.httpClient = httpClient;
         this.aurelia = aurelia;
-
         this.token = localStorage[Const.TOKEN_HEADER];
     }
 
     public login(username:string, password:string):void {
+        let authToken = '';
         this.httpClient
-            .fetch(Const.BASE_URL + Const.LOGIN_URL, {
+            .fetch(BASE_URL + LOGIN, {
                 method: 'post',
                 headers: {
                     Username: username,
                     Password: password
                 }
             })
-            .then((response) => {
-                let statusCode = response.status;
-                let authToken = response.headers.get(Const.TOKEN_HEADER);
-                if (statusCode == 200) {
+            .then(response => {
+                authToken = response.headers.get(Const.TOKEN_HEADER);
+                return response.json();
+            })
+            .then(data => {
+                let statusCode = data.status;
+                if (this.isStatusCodeOk(statusCode)) {
                     localStorage[Const.TOKEN_HEADER] = authToken;
                     this.token = authToken;
                     this.aurelia.setRoot(Const.APP_ROOT);
+                } else {
+                    alert(data.message);
                 }
+            });
+
+    }
+
+    register(username:string, password:string, email:string, gender:Twitter.Models.Gender) {
+        this.httpClient
+            .fetch(BASE_URL + REGISTER, {
+                method: 'post',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: json({
+                    "username": username,
+                    "password": password,
+                    "email": email,
+                    "gender": gender
+                })
             })
+            .then(response => response.json())
+            .then(data => {
+                let statusCode = data.status;
+                if (this.isStatusCodeOk(statusCode)) {
+                    alert('Account has been created!');
+                } else {
+                    alert(data.message);
+                }
+            });
+
     }
 
 
     public isAuthenticated():boolean {
         return !(this.token == null);
+    }
+
+    private isStatusCodeOk(statusCode:number | string):boolean {
+        return statusCode > 200 && statusCode < 300;
     }
 
 }
--- src/main/resources/static/src/register.ts
+++ src/main/resources/static/src/register.ts
@@ -1,9 +1,33 @@
+import {inject} from "aurelia-dependency-injection";
+import {AuthServiceImpl, AuthService} from "./authService";
+
 /**
  * Created by mariusz on 24.08.16.
  */
 
-
+@inject(AuthServiceImpl)
 export class Register {
-    
+
+    authService:AuthService;
+    username:string;
+    password:string;
+    passwordConfirm:string;
+    email:string;
+    emailConfirm:string;
+    gender:Twitter.Models.Gender;
+
+    constructor(authService:AuthService) {
+        this.authService = authService;
+    }
+
+    register() {
+        if (this.password !== this.passwordConfirm) {
+            alert('Passwords arent equal!');
+        } else if (this.email !== this.emailConfirm) {
+            alert('Emails arent equal!');
+        } else {
+            this.authService.register(this.username, this.password, this.email, this.gender);
+        }
+    }
 
 }
