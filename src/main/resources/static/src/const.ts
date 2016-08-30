9d5bdea1e31bd0e64a917cb2e738d6b4e9904c22
msut <marr994@o2.pl>
2016-08-30 23:30:34 +0200

    moved url to other file

--- src/main/resources/static/src/const.ts
+++ src/main/resources/static/src/const.ts
@@ -2,10 +2,7 @@
  * Created by mariusz on 23.08.16.
  */
 export class Const {
-    public static BASE_URL:string = 'http://localhost:8080/';
-    public static LOGIN_URL:string = 'api/login';
     public static TOKEN_HEADER:string = 'Auth-Token';
     public static UNAUTHORIZE_ROOT:string = 'unauthorize';
     public static APP_ROOT:string = 'app';
 }
-;
---
+++ src/main/resources/static/src/route.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by mariusz on 30.08.16.
+ */
+
+export const BASE_URL = `http://localhost:8080`;
+export const REGISTER = `/api/user`;
+export const LOGIN = `/api/login`;
+
