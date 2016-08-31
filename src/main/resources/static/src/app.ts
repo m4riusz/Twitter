70bc9ec163da9738651f87dd9b5a777949878500
msut <marr994@o2.pl>
2016-08-31 20:48:59 +0200

    add simple router

--- src/main/resources/static/src/app.ts
+++ src/main/resources/static/src/app.ts
@@ -1,10 +1,18 @@
+import {RouterConfiguration, Router} from "aurelia-router";
 /**
  * Created by mariusz on 22.08.16.
  */
-export class App {
-    private message:string;
 
-    constructor() {
-        this.message = "Hello Aurelia 4";
+export class App {
+
+    public router:Router;
+
+    configureRouter(config:RouterConfiguration, router:Router) {
+
+        config.map([
+            {route: ['', 'home'], name: 'home', moduleId: 'home', title: 'Home', nav: true},
+            {route: ['tweets'], name: 'tweets', moduleId: 'tweets', title: 'Tweets', nav: true}
+        ]);
+        this.router = router;
     }
 }
---
+++ src/main/resources/static/src/tweets.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by mariusz on 31.08.16.
+ */
+
+export class Tweets{
+    
+}
