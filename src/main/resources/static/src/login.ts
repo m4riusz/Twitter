5a8d4cf2f39e073ce65f6b468193d59e97de5c4b
msut <marr994@o2.pl>
2016-09-01 23:06:32 +0200

    refactor login

--- src/main/resources/static/src/login.ts
+++ src/main/resources/static/src/login.ts
@@ -1,35 +1,53 @@
 import {AuthService, AuthServiceImpl} from "./authService";
 import {inject} from "aurelia-dependency-injection";
 import {Const} from "./const";
+import {Aurelia} from "aurelia-framework";
 
 /**
  * Created by mariusz on 23.08.16.
  */
 
-@inject(AuthServiceImpl)
+@inject(AuthServiceImpl, Aurelia)
 export class Login {
-    authService:AuthService;
+    private authService:AuthService;
+    private aurelia:Aurelia;
+    private token:string;
+
     username:string;
     password:string;
+    error:string;
 
-    constructor(authService:AuthService) {
+    constructor(authService:AuthService, aurelia:Aurelia) {
         this.authService = authService;
+        this.aurelia = aurelia;
+        this.token = localStorage[Const.TOKEN_HEADER];
     }
 
-    public login(username:string, password:string):void {
-        if (this.usernameHasValidLength() && this.passwordHasValidLength()) {
-            this.authService.login(this.username, this.password)
-        } else {
-            alert(`Ensure that username length is between ${Const.LOGIN_LENGTH.MIN} and ${Const.LOGIN_LENGTH.MAX} and
-            password length is between ${Const.PASSWORD_LENGTH.MIN} and ${Const.PASSWORD_LENGTH.MAX}`);
-        }
+    activate() {
+        this.username = '';
+        this.password = '';
+        this.error = '';
     }
 
-    public usernameHasValidLength():boolean {
-        return this.username !== undefined && this.username.length >= Const.LOGIN_LENGTH.MIN && this.username.length <= Const.LOGIN_LENGTH.MAX;
+    public login():void {
+        this.authService.login(this.username, this.password)
+            .then(response => {
+                let authToken = response.headers.get(Const.TOKEN_HEADER);
+                if (response.ok) {
+                    localStorage[Const.TOKEN_HEADER] = authToken;
+                    this.token = authToken;
+                    this.aurelia.setRoot(Const.APP_ROOT);
+                    return;
+                }
+                response.json()
+                    .then(data => {
+                        this.error = data.message;
+                    })
+            })
     }
 
-    public passwordHasValidLength():boolean {
-        return this.password !== undefined && this.password.length >= Const.PASSWORD_LENGTH.MIN && this.password.length <= Const.PASSWORD_LENGTH.MAX;
+    public isAuthenticated():Promise<boolean> {
+        return this.authService.isTokenValid(this.token);
     }
+
 }
