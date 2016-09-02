c76474ae6d8d76f639a557fb81c51957734b0a6e
msut <marr994@o2.pl>
2016-09-02 23:44:41 +0200

    add current logged user VM

--- src/main/resources/static/src/app.ts
+++ src/main/resources/static/src/app.ts
@@ -1,12 +1,28 @@
 import {RouterConfiguration, Router} from "aurelia-router";
+import {UserService, UserServiceImpl} from "./userService";
+import {inject} from "aurelia-dependency-injection";
 /**
  * Created by mariusz on 22.08.16.
  */
 
+@inject(UserServiceImpl)
 export class App {
 
+    public loggedUser:User;
     public router:Router;
+    private userService:UserService;
 
+    constructor(userService:UserService) {
+        this.userService = userService;
+    }
+
+    activate() {
+        this.userService.getCurrentLoggedUser()
+            .then(response => response.json())
+            .then(data => {
+                this.loggedUser = data;
+            })
+    }
     configureRouter(config:RouterConfiguration, router:Router) {
 
         config.map([
