39679b6f5fcef4ca2dcb2e412802de086852b521
msut <marr994@o2.pl>
2016-09-03 21:35:17 +0200

    add home page view

--- src/main/resources/static/src/home.ts
+++ src/main/resources/static/src/home.ts
@@ -1,7 +1,24 @@
+import {TweetService, TweetServiceImpl} from "./tweetService";
+import {Const} from "./const";
+import {inject} from "aurelia-dependency-injection";
 /**
  * Created by mariusz on 31.08.16.
  */
-
+@inject(TweetServiceImpl)
 export class Home{
-    
+    pageNumber:number;
+    tweets:Tweet[];
+    tweetService:TweetService;
+
+    constructor(tweetService:TweetService) {
+        this.pageNumber = 0;
+        this.tweetService = tweetService;
+    }
+
+    activate() {
+        this.tweetService.getAllTweets(this.pageNumber, Const.PAGE_SIZE)
+            .then(data => {
+                this.tweets = data;
+            })
+    }
 }
