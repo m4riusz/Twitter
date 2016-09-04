068fe697e9c5b0603216a5541b9457e70de32c51
msut <marr994@o2.pl>
2016-09-04 23:43:25 +0200

    add delete tweet method

--- src/main/resources/static/src/route.ts
+++ src/main/resources/static/src/route.ts
@@ -7,6 +7,7 @@ export const REGISTER = `/api/user`;
 export const CURRENT_USER = `/api/user`;
 export const LOGIN = `/api/login`;
 export const TWEET_URL = `/api/tweet`;
+export const TWEET_BY_ID = (tweetId:number) => `${TWEET_URL}/${tweetId}`;
 export const TWEET_GET_ALL = (page:number, size:number) => `${TWEET_URL}/${page}/${size}`;
 
 
--- src/main/resources/static/src/tweetService.ts
+++ src/main/resources/static/src/tweetService.ts
@@ -2,7 +2,7 @@ import Tweet = Twitter.Models.Tweet;
 import UserVote = Twitter.Models.UserVote;
 import {HttpClient, json} from "aurelia-fetch-client";
 import {inject} from "aurelia-dependency-injection";
-import {BASE_URL, TWEET_URL, TWEET_GET_ALL} from "./route";
+import {BASE_URL, TWEET_URL, TWEET_GET_ALL, TWEET_BY_ID} from "./route";
 import {Const} from "./const";
 /**
  * Created by mariusz on 01.09.16.
@@ -12,11 +12,12 @@ import {Const} from "./const";
 export interface TweetService {
     create(tweet:Tweet):Promise<Tweet>;
     getAllTweets(page:number, size:number):Promise<Tweet[]>;
+    deleteTweet(tweetId:number):Promise<void>;
+    getTweetById(tweetId:number):Promise<Tweet>;
 }
 
 @inject(HttpClient)
 export class TweetServiceImpl implements TweetService {
-
     private httpClient:HttpClient;
     private authToken:string;
 
@@ -28,8 +29,8 @@ export class TweetServiceImpl implements TweetService {
     create(tweet:Tweet):Promise<Tweet> {
         return new Promise((resolve, reject) => {
             this.httpClient.fetch(BASE_URL + TWEET_URL, {
+                method: 'post',
                 headers: {
-                    method: 'post',
                     [Const.TOKEN_HEADER]: this.authToken,
                     body: json(tweet)
                 }
@@ -48,7 +49,6 @@ export class TweetServiceImpl implements TweetService {
         return new Promise((resolve, reject) => {
             this.httpClient.fetch(BASE_URL + TWEET_GET_ALL(page, size), {
                 headers: {
-                    method: 'get',
                     [Const.TOKEN_HEADER]: this.authToken
                 }
             })
@@ -62,5 +62,32 @@ export class TweetServiceImpl implements TweetService {
         });
     }
 
+    deleteTweet(tweetId:number):Promise<> {
+        return new Promise((resolve, reject) => {
+            this.httpClient.fetch(BASE_URL + TWEET_BY_ID(tweetId), {
+                method: 'delete',
+                headers: {
+                    [Const.TOKEN_HEADER]: this.authToken
+                }
+            })
+                .then(response => {
+                    response.ok ? resolve() : reject();
+                })
+        });
+    }
 
+    getTweetById(tweetId:number):Promise<Tweet> {
+        return new Promise((resolve, reject) => {
+            this.httpClient.fetch(BASE_URL + TWEET_BY_ID(tweetId), {
+                headers: {
+                    [Const.TOKEN_HEADER]: this.authToken
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    resolve(data);
+                })
+        })
+
+    }
 }
