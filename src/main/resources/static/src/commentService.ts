107828927ff660abff533167438bcb218c0e112d
msut <marr994@o2.pl>
2016-09-15 20:39:51 +0200

    add vote comment methods

--- src/main/resources/static/src/commentService.ts
+++ src/main/resources/static/src/commentService.ts
@@ -2,8 +2,9 @@ import {HttpClient} from "aurelia-fetch-client";
 import {inject} from "aurelia-dependency-injection";
 import {BasicService} from "./basicService";
 import {Const} from "./const";
-import {BASE_URL, COMMENTS_FROM_TWEET} from "./route";
+import {BASE_URL, COMMENTS_FROM_TWEET, COMMENT_VOTE_BY_ID, COMMENT_VOTE} from "./route";
 import Comment = Models.Comment;
+import Vote = Models.Vote;
 
 /**
  * Created by mariusz on 14.09.16.
@@ -11,6 +12,9 @@ import Comment = Models.Comment;
 
 export interface ICommentService {
     getTweetComments(tweetId:number, page:number, size:number):Promise<Comment[]>;
+    getCurrentUserCommentVote(commentId:number):Promise<Vote>;
+    voteComment(commentId:number, vote:Vote):Promise<Vote>;
+    deleteCommentVote(commentId:number):Promise<{}>;
 }
 
 @inject(HttpClient)
@@ -33,9 +37,61 @@ export class CommentService extends BasicService implements ICommentService {
             })
                 .then(response=> response.json())
                 .then((comments:Comment[]) => {
-                    console.log(comments);
+                    comments.forEach(comment => {
+                        this.getCommentCurrentUserData(comment);
+                    });
                     resolve(comments);
                 }, (error) => resolve([]));
         });
+    }
+
+    getCurrentUserCommentVote(commentId:number):Promise<Vote> {
+        return new Promise<Vote>((resolve, reject) => {
+            this.httpClient.fetch(BASE_URL + COMMENT_VOTE_BY_ID(commentId), {
+                headers: {
+                    [Const.TOKEN_HEADER]: this.authToken
+                }
+            })
+                .then(response => response.json())
+                .then((data:UserVote)=> {
+                    resolve(data.vote);
+                }, ()=> {
+                    resolve("NONE");
+                });
+        })
+    }
+
+    voteComment(commentId:number, vote:Vote):Promise<Vote> {
+        return new Promise<Vote>((resolve, reject) => {
+            this.httpClient.fetch(BASE_URL + COMMENT_VOTE, {
+                method: 'post',
+                body: json({'postId': commentId, 'vote': vote}),
+                headers: {
+                    [Const.TOKEN_HEADER]: this.authToken
+                }
+            })
+                .then(response => response.json())
+                .then((userVote:UserVote) => {
+                    resolve(userVote.vote);
+                });
+        });
+    }
+
+    deleteCommentVote(commentId:number):Promise<{}> {
+        return new Promise<{}>((resolve, reject) => {
+            this.httpClient.fetch(BASE_URL + COMMENT_VOTE_BY_ID(commentId), {
+                method: 'delete',
+                headers: {
+                    [Const.TOKEN_HEADER]: this.authToken
+                }
+            })
+                .then(()=> {
+                    resolve();
+                })
+        })
+    }
+
+    private getCommentCurrentUserData(comment:Comment) {
+        this.getCurrentUserCommentVote(comment.id).then((vote:Vote) => comment.loggedUserVote = vote);
     }
 }
