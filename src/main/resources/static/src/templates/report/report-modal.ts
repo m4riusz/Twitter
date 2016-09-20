26eda7355b8416af209738d04cec1c3983bc942b
msut <marr994@o2.pl>
2016-09-20 21:24:46 +0200

    add report modal

--- src/main/resources/static/src/templates/report/report-modal.ts
+++ src/main/resources/static/src/templates/report/report-modal.ts
@@ -1,5 +1,6 @@
 import {inject} from "aurelia-dependency-injection";
 import {DialogController} from "aurelia-dialog";
+import {Const} from "../../domain/const";
 
 /**
  * Created by mariusz on 19.09.16.
@@ -8,15 +9,29 @@ import {DialogController} from "aurelia-dialog";
 @inject(DialogController)
 export class ReportModal {
     message:string;
+    maxLength:number;
     private dialogController:DialogController;
+    private categories:Object[];
+    private selectedCategory:Object;
 
     constructor(dialogController:DialogController) {
         this.dialogController = dialogController;
+        this.categories = [
+            {id: 0, text: "Verbal abuse"},
+            {id: 1, text: "Hate speech"},
+            {id: 2, text: "Pornography"},
+            {id: 3, text: "Advertisement"},
+            {id: 4, text: "Spam or flood"},
+            {id: 5, text: "Wrong tags"},
+            {id: 6, text: "Other"}
+        ];
+        this.selectedCategory = this.categories[0];
         dialogController.settings.centerHorizontalOnly = true;
     }
 
     activate(message) {
-        this.message = message;
+        typeof message == 'undefined' ? this.message = '' : this.message = message;
+        this.maxLength = Const.REPORT_MESSAGE_MAX_LENGTH;
     }
 
 }
