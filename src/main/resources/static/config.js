6113d6dc2c6201cadc645c7c671ad2c3e99f94a4
msut <marr994@o2.pl>
2016-10-07 22:30:24 +0200

    add jqueryui for datepicker

--- src/main/resources/static/config.js
+++ src/main/resources/static/config.js
@@ -34,6 +34,12 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.7",
+    "jquery": "npm:jquery@3.1.1",
+    "jquery-ui": "github:components/jqueryui@1.12.1",
+    "moment": "npm:moment@2.15.1",
+    "github:components/jqueryui@1.12.1": {
+      "jquery": "npm:jquery@3.1.1"
+    },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
