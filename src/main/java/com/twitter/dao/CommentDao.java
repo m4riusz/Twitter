cefbf61187f9f04b0dc2e19b241bd587ba9eabe7
msut <marr994@o2.pl>
2016-11-15 21:22:22 +0100

    fix comment order

--- src/main/java/com/twitter/dao/CommentDao.java
+++ src/main/java/com/twitter/dao/CommentDao.java
@@ -3,9 +3,12 @@ package com.twitter.dao;
 import com.twitter.model.Comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.twitter.dao.Query.SELECT_COMMENT_FROM_TWEET_ORDER_BY_VOTE_SIZE;
 
 /**
  * Created by mariusz on 21.07.16.
@@ -19,7 +22,8 @@ public interface CommentDao extends JpaRepository<Comment, Long> {
 
     List<Comment> findByTweetIdOrderByCreateDateDesc(long tweetId, Pageable pageable);
 
-    List<Comment> findByTweetIdOrderByVotesAscCreateDateDesc(long tweetId, Pageable pageable);
-
     List<Comment> findByOwnerId(long userId, Pageable pageable);
+
+    @Query(value = SELECT_COMMENT_FROM_TWEET_ORDER_BY_VOTE_SIZE)
+    List<Comment> findByTweetIdOrderByVotesAscCreateDateDesc(long tweetId, Pageable pageable);
 }
