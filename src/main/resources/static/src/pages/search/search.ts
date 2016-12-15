6fdb9b2686a9faeaaad1507927cf561d340c167a
msut <marr994@o2.pl>
2016-12-15 19:48:15 +0100

    change query form

--- src/main/resources/static/src/pages/search/search.ts
+++ src/main/resources/static/src/pages/search/search.ts
@@ -1,8 +1,24 @@
+import User = Models.User;
+import Tag = Models.Tag;
 /**
  * Created by mariusz on 14.12.16.
  */
 
 
 export class SearchVM {
-    
+
+    inputText:string;
+    selected:string;
+    result:User[]|Tag[];
+    private options:string[];
+
+    constructor() {
+        this.options = ["Tag", "User"];
+        this.selected = this.options[0];
+    }
+
+    query() {
+        console.log("query");
+        console.log(this.selected);
+    }
 }
